2e0591014dde7a6964c808fcac690ab27e35d890
Amanda Zaine <amandazaine@outlook.com>
2022-08-28 15:00:18 -0300

    segundo commit da Zai

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 //Alteração realizada por Amanda Zaine
-
+//Outra alteração realizada por Amanda Zaine
 Console.WriteLine("Hello, World!");
